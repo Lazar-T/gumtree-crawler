fff4fb31dc9ca4fdfeace3bcd43e1d810cca6dfe
Lazar-T <lazartelebak@gmail.com>
2015-07-19 19:21:27 +0200

    Update middlewares.py

--- gumtree/middlewares.py
+++ gumtree/middlewares.py
@@ -5,7 +5,7 @@ from scrapy.conf import settings
 
 class RandomUserAgentMiddleware(object):
     def process_request(self, request, spider):
-        ua  = random.choice(settings.get('USER_AGENT_LIST'))
+        ua = random.choice(settings.get('USER_AGENT_LIST'))
         if ua:
             request.headers.setdefault('User-Agent', ua)
 
